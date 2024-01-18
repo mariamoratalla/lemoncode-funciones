4d839039150fc52f91a34ff9d71cd121323e849a
mariamoratalla <maria.moratalla97@gmail.com>
2024-01-18 18:25:38 +0100

    ejercicio básico

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,37 @@
-import "./style.css";
+let numeroActual: number = 1;
+const turno: HTMLElement | null = document.querySelector(".numero-turno");
 
-console.log("Hello Typescript!");
+function actualizarTurno(): void {
+  if (turno) {
+    turno.textContent = numeroActual.toString().padStart(2, "0");
+  }
+}
+
+function turnoSiguiente(): void {
+  numeroActual++;
+  actualizarTurno();
+}
+
+function turnoAnterior(): void {
+  if (numeroActual > 1) {
+    numeroActual--;
+    actualizarTurno();
+  }
+}
+
+function resetTurno(): void {
+  numeroActual = 1;
+  actualizarTurno();
+}
+
+const siguiente = document.getElementById("siguiente");
+const anterior = document.getElementById("anterior");
+const reset = document.getElementById("reset");
+
+siguiente?.addEventListener("click", turnoSiguiente);
+anterior?.addEventListener("click", turnoAnterior);
+reset?.addEventListener("click", resetTurno);
+
+actualizarTurno();
+
+
